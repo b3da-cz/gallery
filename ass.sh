6536e78277a2b2b844199c23f1503a4a70d2f0f8
Tomas Beran <b3da@b3da.net>
2017-10-23 16:18:23 +0200

    Fix ass.sh

--- ass.sh
+++ ass.sh
@@ -4,14 +4,14 @@ chmod -R 0777 var
 if [ "$1" = "-f" ]
 then
     echo "force updating schema"
-    app/console d:s:u --dump-sql --force
+    bin/console d:s:u --dump-sql --force
 fi
-app/console cache:clear --env=dev
-app/console cache:clear --env=prod
-app/console assets:install --symlink
+bin/console cache:clear --env=dev
+bin/console cache:clear --env=prod
+bin/console assets:install --symlink
 echo '------------------------------------------------------------'
 echo '------------------------------------------------------------'
-app/console d:s:u --dump-sql
+bin/console d:s:u --dump-sql
 echo '------------------------------------------------------------'
 echo 'you are good to go! (or run again with -f for schema update)'
 chmod -R 0777 var
